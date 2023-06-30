d749409d305691cddb8cfa0afb1e07b4b63ca61a
Nneoma <nneoma.amy@gmail.com>
2023-06-30 18:18:30 +0000

    changing minimax to send tuples

--- student/src/tictactoe_game_ai.ml
+++ student/src/tictactoe_game_ai.ml
@@ -80,7 +80,9 @@ let score
   let score =
     match eval with
     | Tic_tac_toe_exercises_lib.Evaluation.Game_over { winner = Some piece }
-      -> (* if my piece wins, we get positive infinity; If the other team wins we get negative infinity; Right now anything else will return 0.0 *)
+      ->
+      (* if my piece wins, we get positive infinity; If the other team wins
+         we get negative infinity; Right now anything else will return 0.0 *)
       (match Piece.equal piece me with
        | true -> Float.infinity
        | false -> Float.neg_infinity)
@@ -89,47 +91,108 @@ let score
   score
 ;;
 
-
-let sample_pieces ~pieces ~me pos  = 
+let sample_pieces ~pieces ~me pos =
   let pieces = Map.set pieces ~key:pos ~data:me in
   pieces
 ;;
 
-let compare_float_pos (t1 : (float, Position.t))  = 
-  fun(a1, _) -> (a2, _) -> Float.compare a1 a2
+let compare_float_pos
+  ((f1, p1) : float * Position.t)
+  ((f2, p2) : float * Position.t)
+  =
+  ignore p1;
+  ignore p2;
+  Float.compare f1 f2
 ;;
 
-(* takes in a Position, a depth, and if current piece is the maximizing player to return the 
-   highest score possible to anticipate which position would yield the best results*)
-let rec minimax ~pos ~(pieces_old : Piece.t Position.Map.t) ~game_kind ~me ~(d : int) ~(maxPlayer : bool) = 
-  (* pieces_new is the Map game_State where we decide to make the decision of going through with the given pos*)
+let found_position pos avail_pos = not (Position.equal pos avail_pos)
+
+let max_float_pos ((f1, p1) : float * Position.t) ((f2, p2) : float * Position.t) =
+  if Float.compare f1 f2 >= 0 then (f1,p1) else (f2,p2)
+;;
+
+let min_float_pos ((f1, p1) : float * Position.t) ((f2, p2) : float * Position.t) =
+  if Float.compare f1 f2 <= 0 then (f1,p1) else (f2,p2)
+;;
+
+(* takes in a Position, a depth, and if current piece is the maximizing
+   player to return the highest score possible to anticipate which position
+   would yield the best results*)
+let rec minimax
+  ~pos
+  ~(pieces_old : Piece.t Position.Map.t)
+  ~game_kind
+  ~me
+  ~availible_pos
+  ~(d : int)
+  ~(maxPlayer : bool)
+  =
+  (* pieces_new is the Map game_State where we decide to make the decision of
+     going through with the given pos*)
   let pieces_new = Map.set pieces_old ~key:pos ~data:me in
-  (* pos is terminal node [no children] -> game ends by a win, loss, or tie *)
-  if d = 0 || (match (Tic_tac_toe_exercises_lib.evaluate ~game_kind ~pieces:pieces_new) with | Tic_tac_toe_exercises_lib.Evaluation.Game_over {winner = Some piece } -> true | _ -> false) 
-    then (score ~me ~game_kind ~pieces:pieces_new, pos)
-else (
-  (* a list of available positions that are unfilled *)
-  let available_pos = Tic_tac_toe_exercises_lib.available_moves ~game_kind ~pieces:pieces_new in
-  (* a list of Map Pieces with the same indices as positions *)
-  let possible_pieces = List.map available_pos ~f:(sample_pieces ~pieces:pieces_new ~me) in
-  if maxPlayer then (
-    let value = Float.neg_infinity in
-  (* the List.map should return a list of scores for each potential position that can be placed *)
-    let list_of_scores = List.map possible_pieces ~f:(fun p -> (minimax ~pieces:p ~game_kind ~me ~d:(d - 1) ~maxPlayer:false)) in
-    (* this is going to pick the maximum score of the list of scores and compares it to value *)
-    let max_score : float = match (List.max_elt list_of_scores ~compare:compare_float_pos) with | None -> Float.neg_infinity | Some float -> float in 
-    let value = Float.max value max_score in 
+  (* we update the availible position list with the position that we used
+     removed *)
+  let available_pos = List.filter availible_pos ~f:(found_position pos) in
+  (* pos is terminal node [no children] -> game ends by a win, loss, or
+     tie *)
+  if d = 0
+     ||
+     match
+       Tic_tac_toe_exercises_lib.evaluate ~game_kind ~pieces:pieces_new
+     with
+     | Tic_tac_toe_exercises_lib.Evaluation.Game_over { winner = Some piece }
+       ->
+       true
+     | _ -> false
+  then (score ~me ~game_kind ~pieces:pieces_new, pos)
+  else if maxPlayer
+  then (
+    let value = Float.neg_infinity, pos in
+    (* the List.map should return a list of scores for each potential
+       position that can be placed *)
+    let list_of_scores : (float * Position.t) list =
+      List.map available_pos ~f:(fun p ->
+        minimax
+          ~pos:p
+          ~pieces_old:pieces_new
+          ~game_kind
+          ~me
+          ~availible_pos
+          ~d:(d - 1)
+          ~maxPlayer:false)
+    in
+    (* this is going to pick the maximum score of the list of scores and
+       compares it to value *)
+    let max_score : float * Position.t =
+      match List.max_elt list_of_scores ~compare:compare_float_pos with
+      | None -> Float.neg_infinity, pos
+      | Some (float, Position.t) -> float, Position.t
+    in
+    let value = max_float_pos value max_score in
     value)
-  else ( (* if it is the minimizing player *)
-    let value = Float.infinity in (* do i put me or Me.flip? i think still me *)
-    let list_of_scores = List.map possible_pieces ~f:(fun p -> (minimax ~pieces:p ~game_kind ~me ~d:(d - 1) ~maxPlayer:true)) in
-    let min_score : float = match (List.min_elt list_of_scores ~compare:Float.compare) with | None -> Float.infinity | Some float -> float in 
-    let value = Float.min value min_score in 
-    value
-  ))
+  else (
+    (* if it is the minimizing player *)
+    let value = Float.infinity in
+    (* do i put me or Me.flip? i think still me *)
+    let list_of_scores =
+      List.map possible_pieces ~f:(fun p ->
+        minimax
+        ~pos:p
+        ~pieces_old:pieces_new
+        ~game_kind
+        ~me
+        ~availible_pos
+          ~d:(d - 1)
+          ~maxPlayer:true)
+    in
+    let min_score  =
+      match List.min_elt list_of_scores ~compare:Float.compare with
+      | None -> Float.infinity, pos
+      |  Some (float, Position.t) -> float, Position.t
+    in
+    let value = min_float_pos value min_score in
+    value)
 ;;
-
-
 
 (* [compute_next_move] is your Game AI's function.
 
@@ -142,19 +205,20 @@ else (
    available spots to pick. *)
 let compute_next_move ~(me : Piece.t) ~(game_state : Game_state.t)
   : Position.t
-  = (* depth is how many moves user  wants to look in the future) *)
+  =
+  (* depth is how many moves user wants to look in the future) *)
   let pieces = game_state.pieces in
   let game_kind = game_state.game_kind in
+  let available_pos = Tic_tac_toe_exercises_lib.available_moves ~game_kind ~pieces in
   (* pos is a random Position of possible places AI can place its piece in *)
   (* let us try to see 3 moves in the future*)
 
-
   (* apply minimax on the children instead of at the root*)
-  let pos = minimax ~pieces ~game_kind ~me ~d:3 ~maxPlayer:true
-
-
-  (* let pos = (* minimax current_pos 4 true -> returns move we should go to next *)
-    pick_winning_move_or_block_if_possible_strategy ~me ~game_kind ~pieces *)
+  let score_pos =
+    minimax ~pos: ~pieces_old:pieces ~game_kind ~me ~d:3 ~maxPlayer:true
+    (* let pos = (* minimax current_pos 4 true -> returns move we should go
+       to next *) pick_winning_move_or_block_if_possible_strategy ~me
+       ~game_kind ~pieces *)
   in
   pos
 ;;
